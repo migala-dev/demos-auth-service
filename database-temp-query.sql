03fb591b8cce151404ae86778a4b2413b53685f1
Alex Ortega <alexortegazj@gmail.com>
2021-08-21 12:54:20 -0700

    Updating databse query

--- database-temp-query.sql
+++ database-temp-query.sql
@@ -1,5 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE users (
     user_id uuid DEFAULT uuid_generate_v4 (),
     name varchar(255),
@@ -10,6 +18,11 @@ CREATE TABLE users (
     updated_at timestamp not null default CURRENT_TIMESTAMP,
 	PRIMARY KEY(user_id)
 );
+
+CREATE TRIGGER set_timestamp_users
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TABLE spaces (
     space_id uuid DEFAULT uuid_generate_v4 (),
@@ -24,6 +37,11 @@ CREATE TABLE spaces (
 	PRIMARY KEY(space_id),
     FOREIGN KEY(owner_id) REFERENCES users(user_id)
 );
+
+CREATE TRIGGER set_timestamp_spaces
+BEFORE UPDATE ON spaces
+FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TABLE user_space (
     user_space_id uuid DEFAULT uuid_generate_v4 (),
@@ -42,6 +60,10 @@ CREATE TABLE user_space (
     FOREIGN KEY(updated_by) REFERENCES users(user_id)
 );
 
+CREATE TRIGGER set_timestamp_user_space
+BEFORE UPDATE ON user_space
+FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
 
 CREATE TABLE role_user_space (
     role_user_space_id uuid DEFAULT uuid_generate_v4 (),
@@ -59,3 +81,8 @@ CREATE TABLE role_user_space (
     FOREIGN KEY(created_by) REFERENCES users(user_id),
     FOREIGN KEY(updated_by) REFERENCES users(user_id)
 );
+
+CREATE TRIGGER set_timestamp_role_user_space
+BEFORE UPDATE ON role_user_space
+FOR EACH ROW
+EXECUTE FUNCTION trigger_set_timestamp();
